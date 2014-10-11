b64ee381f4b5c19299149498df560eb3357b16d2
Skokanek <o.skokanek@seznam.cz>
2014-10-11 10:47:16 +0200

    Start

---
+++ AsyncApp/AsyncApp/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace AsyncApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbOsoby = new System.Windows.Forms.ListBox();
+            this.tbJmeno = new System.Windows.Forms.TextBox();
+            this.tbPrijmeni = new System.Windows.Forms.TextBox();
+            this.dtpNarozen = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbOsoby
+            // 
+            this.lbOsoby.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbOsoby.FormattingEnabled = true;
+            this.lbOsoby.Location = new System.Drawing.Point(12, 15);
+            this.lbOsoby.Name = "lbOsoby";
+            this.lbOsoby.Size = new System.Drawing.Size(317, 472);
+            this.lbOsoby.TabIndex = 0;
+            // 
+            // tbJmeno
+            // 
+            this.tbJmeno.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbJmeno.Location = new System.Drawing.Point(398, 16);
+            this.tbJmeno.Name = "tbJmeno";
+            this.tbJmeno.Size = new System.Drawing.Size(226, 20);
+            this.tbJmeno.TabIndex = 1;
+            // 
+            // tbPrijmeni
+            // 
+            this.tbPrijmeni.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPrijmeni.Location = new System.Drawing.Point(398, 49);
+            this.tbPrijmeni.Name = "tbPrijmeni";
+            this.tbPrijmeni.Size = new System.Drawing.Size(226, 20);
+            this.tbPrijmeni.TabIndex = 2;
+            // 
+            // dtpNarozen
+            // 
+            this.dtpNarozen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpNarozen.Location = new System.Drawing.Point(398, 85);
+            this.dtpNarozen.Name = "dtpNarozen";
+            this.dtpNarozen.Size = new System.Drawing.Size(226, 20);
+            this.dtpNarozen.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(540, 473);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(88, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Uložit změny";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Enabled = false;
+            this.btnExport.Location = new System.Drawing.Point(389, 473);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(126, 23);
+            this.btnExport.TabIndex = 5;
+            this.btnExport.Text = "Exportovat data";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Jméno";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(336, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Příjmení";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Narozen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 503);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dtpNarozen);
+            this.Controls.Add(this.tbPrijmeni);
+            this.Controls.Add(this.tbJmeno);
+            this.Controls.Add(this.lbOsoby);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbOsoby;
+        private System.Windows.Forms.TextBox tbJmeno;
+        private System.Windows.Forms.TextBox tbPrijmeni;
+        private System.Windows.Forms.DateTimePicker dtpNarozen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AsyncApp/AsyncApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace AsyncApp
+{
+    public partial class Form1 : Form
+    {
+        private test5Entities m_context;
+        private BindingSource m_bindingSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;            
+        }       
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            m_context = new test5Entities();
+            m_bindingSource = new BindingSource();
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            m_bindingSource.DataSource = await m_context.Osoba.ToListAsync();
+            lbOsoby.DataSource = m_bindingSource;
+            tbJmeno.DataBindings.Add("Text", m_bindingSource, "Jmeno", false, DataSourceUpdateMode.OnPropertyChanged);
+            tbPrijmeni.DataBindings.Add("Text", m_bindingSource, "Prijmeni", false, DataSourceUpdateMode.OnPropertyChanged);
+            dtpNarozen.DataBindings.Add("Value", m_bindingSource, "DatumNarozeni", true, DataSourceUpdateMode.OnPropertyChanged);
+            btnSave.Enabled = true;
+            btnExport.Enabled = true;
+        }
+
+        private async void SaveData()
+        {
+            btnSave.Enabled = false;
+            btnExport.Enabled = false;
+            await m_context.SaveChangesAsync();
+            btnSave.Enabled = true;
+            btnExport.Enabled = true;
+        }
+
+        private async void ExportToXmlAsync(string path)
+        {
+            btnExport.Enabled = false;
+            await Task.Run(() => ExportToXml(path));
+            btnExport.Enabled = true;
+        }
+
+        private void ExportToXml(string path)
+        {
+            var data = m_context.Osoba.ToList();
+            XDocument doc = new XDocument(new XElement("Osoby"));
+            data.ForEach(o => doc.Root.Add(
+                new XElement("Osoba",
+                    new XElement("Jmeno", o.Jmeno ?? ""),
+                    new XElement("Prijmeni", o.Prijmeni ?? ""),
+                    new XElement("Narozen", o.DatumNarozeni == null ? "" : ((DateTime)o.DatumNarozeni).ToShortDateString()))));
+
+            doc.Save(path);
+        }
+
+        private void ExportData()
+        {
+            var dialog = new SaveFileDialog { DefaultExt = ".xml", Filter = "Xml Files (*.xml)|*.xml" };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ExportToXmlAsync(dialog.FileName);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            ExportData();
+        }        
+    }
+}
